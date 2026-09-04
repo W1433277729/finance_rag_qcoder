c72617c7ce2d91b32e13cb66e7fa493f64116864
王磊 <w1433277729@gmail.com>
2026-09-04 19:19:45 +0800

    feat(master): pdf_to_md节点逻辑完成

--- src/processor/import_processor/node_pdf_to_md.py
+++ src/processor/import_processor/node_pdf_to_md.py
@@ -4,6 +4,7 @@
     @Author :爱吃肯德基
 """
 import os
+import shutil
 import time
 from dataclasses import dataclass
 from pathlib import Path
@@ -17,14 +18,14 @@ from common.logging.logger import step_log
 from utils.path_util import PROJECT_ROOT
 from common.logging.logger import node_log, logger
 from utils.task_utils import add_running_task, add_done_task
-from processor.import_processor.state import ImportGraphState
+from processor.import_processor.state import ImportGraphState, create_default_state
 
 # 提前加载.env配置文件（必须在读取环境变量前执行，确保os.getenv能获取到值）
-# 若.env不在项目根目录，可指定路径：loa		d_dotenv(dotenv_path=Path(__file__).parent / ".env")
+# 若.env不在项目根目录，可指定路径：load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 load_dotenv()
 
 
-# 定义minerU服务配置
+# 定义MinerU服务配置
 @dataclass
 class MineruConfig:
     base_url: str
@@ -56,6 +57,12 @@ def node_pdf_to_md(state: ImportGraphState) -> ImportGraphState:
     pdf_path, output_file_path = step_1_validate_paths(state)
     # step2 MinerU交互，上传和轮询
     zip_url = step_2_upload_and_poll(pdf_path)
+
+    # step3 下载解压
+    md_path = step_3_download_and_extract(zip_url, output_file_path, pdf_path.stem)
+
+    state['md_path'] = str(md_path)
+
     add_done_task(state['task_id'], 'node_pdf_to_md')
     return state
 
@@ -107,11 +114,11 @@ def step_2_upload_and_poll(pdf_path: Path) -> str:
     :param pdf_path: pdf 路径对象
     :return: zip_url
     """
-    # 1. 校验MinerU 参数
+    # ---------------- 1. 校验MinerU 参数 -------------------
     if not mineru_config.base_url or not mineru_config.api_token:
         logger.error("MinerU 配置错误,请检查MinerU 配置!")
         raise ValueError('MinerU 配置错误,请检查MinerU 配置!')
-    # 2. 申请上传地址和批量id
+    # --------------2. 申请上传地址和批量id -----------------
     header = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {mineru_config.api_token}"
@@ -137,12 +144,12 @@ def step_2_upload_and_poll(pdf_path: Path) -> str:
         try:
             poll_response = requests.get(poll_url, headers=header)
         except Exception as e:
-            logger.warning(f"请求出现异常!可以稍后重试!!")
+            logger.warning(f"请求出现异常!{interval_time}秒后重试!!")
             time.sleep(interval_time)
             continue
         # 4.3 判断网络请求状态码
         http_poll_status_code = poll_response.status_code
-        # 不等于200 但是5xx系列给与机会,希望minerU珍惜和修复
+        # 不等于200 但是5xx系列给与机会,希望MinerU珍惜和修复
         if http_poll_status_code != 200:
             if 500 <= http_poll_status_code < 600:
                 # 如果是服务器报错  给机会继续重试
@@ -160,7 +167,7 @@ def step_2_upload_and_poll(pdf_path: Path) -> str:
         # 4.5 判断具体的转化状态 state
         extract_result = poll_response_dict['data']['extract_result'][0]
         extract_result_state = extract_result['state']
-        # 1. done 终结了 获取url地址 2. failed 终结了 失败  3. 其他 进行中 给机会
+        # 1. done 终结了 获取url地址 2. failed 终结了 失败  3. 其他进行中给机会
         if extract_result_state == 'done':
             extract_result_url = extract_result['full_zip_url']
             if not extract_result_url:
@@ -177,6 +184,30 @@ def step_2_upload_and_poll(pdf_path: Path) -> str:
             time.sleep(interval_time)
             continue
 
+
+def step_3_download_and_extract(zip_url, output_file_path:Path, stem) -> Path:
+
+    # ------------------- 1. 下载ZIP ----------------
+    response = requests.get(zip_url)
+    if response.status_code != 200:
+        logger.error(f"从{zip_url}下载文件失败!")
+        raise RuntimeError(f"从{zip_url}下载文件失败!")
+    save_zip_file_path = output_file_path / f"{stem}_result.zip"
+    save_zip_file_path.write_bytes(response.content)
+    # ------------------- 2. 解压ZIP ----------------
+    unzip_path = output_file_path / stem
+    # 如果文件已存在，删除重建，防止旧文件干扰
+    if unzip_path.is_dir():
+        shutil.rmtree(unzip_path)
+    unzip_path.mkdir(parents=True)
+    shutil.unpack_archive(save_zip_file_path, unzip_path)
+    # ------------------- 3. 查找 md 文件 ----------------
+    md_file_list = list(unzip_path.rglob('*.md'))
+    target_md_obj = md_file_list[0]
+    # 重命名
+    final_md_path_obj = target_md_obj.rename(target_md_obj.with_name(f"{stem}.md"))
+    logger.info(f"文件{save_zip_file_path}解压成功")
+    return final_md_path_obj
 
 
 def upload_file(file_upload_url, pdf_path: Path):
@@ -207,7 +238,7 @@ def apply_upload_url_batch_id(pdf_path: Path) -> Tuple[Any, Any]:
     :return:
     """
     token = mineru_config.api_token
-    url = mineru_config.base_url
+    url = f"{mineru_config.base_url}/file-urls/batch"
     header = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}"
@@ -259,3 +290,25 @@ def response_parse(response: Response) -> tuple[Any, Any]:
     # 获取批量操作的id，用于轮询获取本次上传解析结果
     batch_id = response_dict['data']['batch_id']
     return batch_id, file_upload_urls
+
+
+if __name__ == "__main__":
+
+    # 单元测试：验证PDF转MD全流程
+    logger.info("===== 开始node_pdf_to_md节点单元测试 =====")
+
+    logger.info(f"测试获取根地址：{PROJECT_ROOT}")
+
+    test_pdf_name = os.path.join("doc", "hak180产品安全手册.pdf")
+    test_pdf_path = os.path.join(PROJECT_ROOT, test_pdf_name)
+
+    # 构造测试状态
+    test_state = create_default_state(
+        task_id="test_pdf2md_task_001",
+        pdf_path=test_pdf_path,
+        local_dir=os.path.join(PROJECT_ROOT, "output")
+    )
+
+    node_pdf_to_md(test_state)
+
+    logger.info("===== 结束node_pdf_to_md节点单元测试 =====")
